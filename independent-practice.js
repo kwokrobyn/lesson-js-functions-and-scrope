443887ac3f9970dcc42988598ff44cefeac862de
Robyn Kwok <robyn.kwok@duke.edu>
2017-05-23 14:39:56 +0800

    Complete

--- independent-practice.js
+++ independent-practice.js
@@ -2,7 +2,13 @@
 // an array of strings. The function should return an array of numbers where each
 // number is the length of the corresponding string.
 
-// your code here
+var lengths = function(arr) {
+  return arr.map(function(el) {
+    return el.length;
+  });
+}
+
+console.log(lengths(["hello", "mr", "dog"]))
 
 // 2. Write a Javascript function called `transmogrifier`
 // This function should accept three arguments, which you can assume will be numbers.
@@ -11,10 +17,14 @@
 // The transmogrified result of three numbers is the product of the first two numbers,
 // raised to the power of the third number.
 
-// your code here
+var transmogrifier = function(num1, num2, num3) {
+  return Math.pow(num1 * num2, num3);
+}
 
 // 3. Write a function `wordReverse` that accepts a single argument, a string. The
 // method should return a string with the order of the words reversed. Don't worry
 // about punctuation.
 
-// your code here
+var wordReverse = function(s) {
+  return s.split("").reverse().join("");
+}
